b750d1153caa4153c3962350597be9b3ec065de4
liuweilin17@qq.com <liuweilin17@qq.com>
2019-02-20 17:03:44 -0500

    add error handler in login.py

--- flaskr/login.py
+++ flaskr/login.py
@@ -8,6 +8,7 @@ from flaskr.models import User
 import hashlib
 import base64
 import uuid
+import traceback
 
 def hash_password(salt, password):
     t_sha = hashlib.sha512()
@@ -16,34 +17,40 @@ def hash_password(salt, password):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if 'user' in session:
-        logout()
+    try:
+        if 'user' in session:
+            logout()
 
-    form = forms.LoginForm()
-    if form.validate_on_submit():
-        valid = True
-        message = ''
-        username = form.username.data
-        password = form.password.data
+        form = forms.LoginForm()
+        if form.validate_on_submit():
+            valid = True
+            message = ''
+            username = form.username.data
+            password = form.password.data
 
-        user = User.query.filter_by(username=username).first()
-        if user:
-            password_hash = hash_password(user.salt, password)
-            user = User.query.filter_by(username=username, password_hash=password_hash).first()
-            if not user:
+            user = User.query.filter_by(username=username).first()
+            if user:
+                password_hash = hash_password(user.salt, password)
+                user = User.query.filter_by(username=username, password_hash=password_hash).first()
+                if not user:
+                    valid = False
+                    message = "Username or Password does not exist"
+            else:
                 valid = False
                 message = "Username or Password does not exist"
-        else:
-            valid = False
-            message = "Username or Password does not exist"
 
-        if valid:
-            session['user']=user.serialize()
-            return redirect(url_for('home'))
-        else:
-            flash(message)
+            if valid:
+                session['user']=user.serialize()
+                return redirect(url_for('home'))
+            else:
+                flash(message)
 
-    return render_template('login.html', form=form)
+        return render_template('login.html', form=form)
+
+    except Exception as e:
+        # print(e)
+        traceback.print_tb(e.__traceback__)
+        return render_template('error.html', msg='something goes wrong~')
 
 @app.route('/logout')
 def logout():
@@ -52,52 +59,64 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if 'user' in session:
-        logout()
-        return redirect(url_for('home'))
+    try:
+        if 'user' in session:
+            logout()
+            return redirect(url_for('home'))
 
-    form = forms.RegisterForm()
-    if form.validate_on_submit():
-        valid = True
-        message = ''
-        username = form.username.data
-        password1 = form.password1.data
-        password2 = form.password2.data
-        user = User.query.filter_by(username=username).first()
-        if user:
-            valid = False
-            message = 'Username has already existed'
-        else:
-            salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
-            salt = salt.decode('utf-8')
-            password_hash = hash_password(salt, password1)
-            user = User(username=username, password_hash=password_hash, salt=salt)
-            db.session.add(user)
-            db.session.commit()
-            message="Sign up successfully!"
-        if valid:
-            flash(message, "success")
-        else:
-            flash(message, "danger")
+        form = forms.RegisterForm()
+        if form.validate_on_submit():
+            valid = True
+            message = ''
+            username = form.username.data
+            password1 = form.password1.data
+            password2 = form.password2.data
+            user = User.query.filter_by(username=username).first()
+            if user:
+                valid = False
+                message = 'Username has already existed'
+            else:
+                salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
+                salt = salt.decode('utf-8')
+                password_hash = hash_password(salt, password1)
+                user = User(username=username, password_hash=password_hash, salt=salt)
+                db.session.add(user)
+                db.session.commit()
+                message="Sign up successfully!"
+            if valid:
+                flash(message, "success")
+            else:
+                flash(message, "danger")
 
-    return render_template('register.html', form=form)
+        return render_template('register.html', form=form)
+
+    except Exception as e:
+        # print(e)
+        traceback.print_tb(e.__traceback__)
+        return render_template('error.html', msg='something goes wrong~')
 
 
 @app.route('/api/register', methods=['POST'])
 def register_api():
-    username = str(request.args.get('name'))
-    password = str(request.args.get('password'))
+    try:
+        username = str(request.args.get('name'))
+        password = str(request.args.get('password'))
 
-    # if user is exist
-    user = User.query.filter_by(username=username).first()
-    if user:
-        return 'Username has already existed'
-    else:
-        salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
-        salt = salt.decode('utf-8')
-        password_hash = hash_password(salt, password)
-        user = User(username=username, password_hash=password_hash, salt=salt)
-        db.session.add(user)
-        db.session.commit()
+        # if user is exist
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return 'Username has already existed'
+        else:
+            salt = base64.urlsafe_b64encode(uuid.uuid4().bytes)
+            salt = salt.decode('utf-8')
+            password_hash = hash_password(salt, password)
+            user = User(username=username, password_hash=password_hash, salt=salt)
+            db.session.add(user)
+            db.session.commit()
 
-    return 'Sign up successfully!'
+        return 'Sign up successfully!'
+
+    except Exception as e:
+        # print(e)
+        traceback.print_tb(e.__traceback__)
+        return 'Fail to sign up!'
