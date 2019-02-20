d3ac5334b83c87fd46b0a8eb57a6a8dc7214d758
liuweilin17@qq.com <liuweilin17@qq.com>
2019-02-20 16:49:36 -0500

    add error handlers in home.py

--- flaskr/home.py
+++ flaskr/home.py
@@ -3,6 +3,7 @@
 from flask import render_template, url_for, session, redirect
 from flaskr import app
 from flaskr.models import Image
+import traceback
 
 def getFaces(img):
     name, type = img.rsplit('.')
@@ -19,7 +20,11 @@ def home():
     if not user:
         return redirect(url_for('login'))
     else:
-        images = Image.query.filter_by(userid=user['userid'])
-        images = [[image.imageid, getThumbs(image.path), image.path, getThumbs(getFaces(image.path)), getFaces(image.path)] for image in images]
-        print(images)
-        return render_template('home.html', images=images)
+        try:
+            images = Image.query.filter_by(userid=user['userid'])
+            images = [[image.imageid, getThumbs(image.path), image.path, getThumbs(getFaces(image.path)), getFaces(image.path)] for image in images]
+            return render_template('home.html', images=images)
+        except Exception as e:
+            # print(e)
+            traceback.print_tb(e.__traceback__)
+            return render_template('error.html', msg='something goes wrong~')
